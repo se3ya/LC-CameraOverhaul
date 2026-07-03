5b56c1a76e9f292e30410c16e6e4ed722269301f
seeya <seeyagm@proton.me>
2026-07-03 21:37:26 +0300

    refactor: move VehicleTracker into Tracking/

---
+++ Plugin/CameraOverhaul/src/Tracking/VehicleTracker.cs
@@ -0,0 +1,43 @@
+using GameNetcodeStuff;
+using HarmonyLib;
+
+namespace CameraOverhaul;
+
+[HarmonyPatch(typeof(VehicleController))]
+internal static class VehicleTracker
+{
+    private static VehicleController? _active;
+
+    [HarmonyPostfix]
+    [HarmonyPatch(nameof(VehicleController.Update))]
+    private static void UpdatePostfix(VehicleController __instance)
+    {
+        PlayerControllerB? lp = StartOfRound.Instance?.localPlayerController;
+        if (IsPlayerInVehicle(lp, __instance))
+            _active = __instance;
+        else if (_active == __instance)
+            _active = null;
+    }
+
+    public static VehicleController? ResolveActive(PlayerControllerB? localPlayer)
+        => IsPlayerInVehicle(localPlayer, _active) ? _active : null;
+
+    public static void ClearCachedActiveIf(PlayerControllerB? localPlayer)
+    {
+        if (!IsPlayerInVehicle(localPlayer, _active))
+            _active = null;
+    }
+
+    public static void Reset() => _active = null;
+
+    private static bool IsPlayerInVehicle(PlayerControllerB? localPlayer, VehicleController? vehicle)
+    {
+        if (localPlayer == null || vehicle == null)
+            return false;
+
+        return vehicle.currentDriver == localPlayer
+            || vehicle.currentPassenger == localPlayer
+            || vehicle.localPlayerInControl
+            || vehicle.localPlayerInPassengerSeat;
+    }
+}
