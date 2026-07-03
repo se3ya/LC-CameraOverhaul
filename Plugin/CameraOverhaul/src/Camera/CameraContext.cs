0edb7198d6a03e1bbc6be940fcbaddb6725a7330
seeya <seeyagm@proton.me>
2026-07-03 21:31:17 +0300

    feat: add leviathan and freeze fields

---
+++ Plugin/CameraOverhaul/src/Camera/CameraContext.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+namespace CameraOverhaul;
+
+internal struct CameraContext
+{
+    public bool isSprinting;
+    public bool isCrouching;
+    public bool inVehicle;
+    public bool isClimbing;
+    public bool isExhausted;
+    public bool isInspectingItem;
+    public bool criticallyInjured;
+    public bool isUsingJetpack;
+    public bool isBeingShocked;
+    public float sprintMeter;
+    public float drunkness;
+    public float insanity;
+    public float poison;
+    public float sinkingValue;
+    public float shipTakeoffPhase;
+    public float shipLandingPhase;
+    public bool inWater;
+    public bool submerged;
+    public float leviathanProximity01;
+    public LeviathanCue leviathanCue;
+    public bool inSnow;
+    public bool shipWithDoorsOpen;
+    public bool hasActiveLight;
+
+    public Vector3 velocity;
+    public Vector3 forwardRelVelocity;
+
+    public double pitch;
+    public double yaw;
+
+    public bool resetSmoothing;
+}
