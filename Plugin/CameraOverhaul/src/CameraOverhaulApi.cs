af34c9437f9e78770cf49108c7acde9c452ca4f5
seeya <116365392+se3ya@users.noreply.github.com>
2026-09-25 23:22:26 +0300

    feat: add AddTrauma, BumpTrauma, AddKick and SkipVanillaShake

--- Plugin/CameraOverhaul/src/CameraOverhaulApi.cs
+++ Plugin/CameraOverhaul/src/CameraOverhaulApi.cs
@@ -12,5 +12,15 @@ public static class CameraOverhaulApi
         set => _externalSuppression = Mathf.Clamp01(value);
     }
 
+    public static void AddTrauma(float amount) => ScreenShakes.AddTrauma(amount);
+
+    public static void BumpTrauma(float level) => ScreenShakes.BumpTrauma(level);
+
+    public static void BumpTrauma(float level, float hold) => ScreenShakes.BumpTrauma(level, hold);
+
+    public static void AddKick(Vector3 strength) => PlayerControllerBPatch.Rig.AddDamageKick(strength);
+
+    public static void SkipVanillaShake() => HUDManagerPatch.SkipThisFrame();
+
     internal static float EffectScale => 1f - _externalSuppression;
 }
